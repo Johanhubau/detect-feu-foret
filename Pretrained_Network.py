64c2425f4ddbb42275d61e537296808cea7b82c8
alexis51151 <alexisdu51151@gmail.com>
2019-02-04 19:41:53 +0100

    Réseau INCEPTIONV3 préentrainé sur imagenet (testé sur mon PC)

--- Pretrained_Network.py
+++ Pretrained_Network.py
@@ -7,10 +7,14 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.models import load_model
 from matplotlib import pyplot as plt
-import modelconfig as cfg
+# import modelconfig as cfg # Pour utiliser notre propre Dataset
+import modelconfigALG_CorsicaDB as cfg# Pour utiliser la Corsica Fire database
 
+# Charger notre réseau
 
+#model = load_model(cfg.paths["save"]+"\Lolipop1")
 
 ## Chargement des datasets
 
@@ -18,7 +22,7 @@ train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
     directory=cfg.paths["traindb"], #2443 elements
-    target_size=(64, 64),
+    target_size=(75, 75),
     color_mode="rgb",
     batch_size=50,
     class_mode="binary",
@@ -28,7 +32,7 @@ train_generator = train_datagen.flow_from_directory(
 
 test_generator = test_datagen.flow_from_directory(
     directory=cfg.paths["testdb"], #320 elements
-    target_size=(64, 64),
+    target_size=(75, 75),
     color_mode="rgb",
     batch_size=50,
     class_mode="binary",
@@ -39,7 +43,7 @@ test_generator = test_datagen.flow_from_directory(
 validate_datagen = ImageDataGenerator(rescale=1./255)
 predict_generator = test_datagen.flow_from_directory(
     directory=cfg.paths["validatedb"],
-    target_size=(64, 64),
+    target_size=(75, 75),
     color_mode="rgb",
     batch_size=1,
     class_mode="binary",
@@ -49,11 +53,11 @@ predict_generator = test_datagen.flow_from_directory(
 
 
 # create the base pre-trained model
-base_model = InceptionV3(weights='imagenet', include_top=False)
+base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(75, 75, 3))
 
 # add a global spatial average pooling layer
 x = base_model.output
-# x = GlobalAveragePooling2D()(x)
+x = Flatten()(x)
 predictions = Dense(1, activation='sigmoid')(x)
 
 # this is the model we will train
@@ -68,10 +72,10 @@ for layer in model.layers[len(model.layers)-3:len(model.layers)]:
 model.compile(optimizer='rmsprop', loss='binary_crossentropy',metrics=['accuracy'])
 # train the model on the new data for a few epochs
 hist = model.fit_generator(train_generator,
-steps_per_epoch=50,
+steps_per_epoch=22,
 epochs=10,
 validation_data=test_generator,
-validation_steps=1)
+validation_steps=5)
 
 plt.plot(hist.epoch, hist.history['val_loss'], label="val_loss")
 plt.plot(hist.epoch, hist.history['loss'], label='loss')
