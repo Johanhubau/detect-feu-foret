7bb5df00683762032b27d8a41b62fb51728f1aef
Geoffroy-Z <44170467+Geoffroy-Z@users.noreply.github.com>
2019-05-06 18:53:02 +0200

    Update Train.py

--- Multiclass/Train.py
+++ Multiclass/Train.py
@@ -35,12 +35,18 @@ def F1(path_image):
     correctly_attributed=nb_classes*[0]
     attributed=nb_classes*[0]
     nb_elements=nb_classes*[0]
+    confusion_matrix= np.zeros((nb_classes,nb_classes))
+    print(confusion_matrix)
+
     for folder in os.listdir(path_image):
         j=labels.get(folder)
+        k=0
+        l=0
+        m=0
+        n=0
         print(j)
         i = 0
-        n = len(os.listdir(path_image + folder))
-        nb_elements[j] = n
+        nb_elements[j] = len(os.listdir(path_image + folder))
         print(folder)
         for elements in os.listdir(path_image + folder):
             img = Image.open(path_image + folder + '/' + elements)
@@ -49,11 +55,21 @@ def F1(path_image):
             y_classes = y_prob.argmax(axis=-1)
             attributed[y_classes[0]] = attributed[y_classes[0]]+1
             if j == y_classes[0]:
-                i=i+1
+                i+=1
+            if 0==y_classes[0]:
+                k+=1
+            elif 1==y_classes[0]:
+                l+=1
+            elif 2==y_classes[0]:
+                m+=1
+            elif 3==y_classes[0]:
+                n+=1
+        confusion_matrix[j,:]+=[k,l,m,n]
         correctly_attributed[j]=i
 
-    print(nb_elements)
-    print(correctly_attributed)
+    print('nb_elements=', nb_elements )
+    print('correctly_attributed=', correctly_attributed, "\n")
+    print('confusion_matrix:\n',confusion_matrix, "\n")
     correctly_attributed = np.asarray(correctly_attributed)
     attributed = np.asarray(attributed)
     nb_elements = np.asarray(nb_elements)
