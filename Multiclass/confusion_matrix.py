19a01f0a75883bfceb9e3ecf9f880fe527cf14b8
alexis51151 <alexisdu51151@gmail.com>
2019-05-08 10:18:44 +0200

    Code to make colorful and easy to understand confusion matrix from our neural network

---
+++ Multiclass/confusion_matrix.py
@@ -0,0 +1,25 @@
+
+
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+"""array = [[70,5,5,0],
+     [7,25,6,0],
+     [0,0,34,0],
+     [0,0,2,5]]
+"""
+array = [[21,1,0,1],
+     [0,0,0,0],
+     [9,3,7,2],
+     [0,0,0,2]]
+array = np.array(array)
+array = (array.T/np.sum(array, axis=1)).T
+df_cm = pd.DataFrame(array, ["Fire","Fog","Not Fire","Red object"],
+                  ["Fire","Fog","Not Fire","Red object"])
+plt.figure(figsize = (10,8))
+sn.set(font_scale=1.4)#for label size
+ax = sn.heatmap(df_cm, annot=True,annot_kws={"size": 16,})# font size
+plt.xlabel('Classe prédite')
+plt.ylabel('Vraie classe')
+plt.show()
