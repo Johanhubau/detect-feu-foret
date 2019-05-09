9af97983dfccc82f23da9318cea173883d668ef2
pharch <matthieuchatard@hotmail.fr>
2019-05-09 15:53:03 +0200

    make working html prototype

--- maquette/main.py
+++ maquette/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def main():
 
-    return render_template('index.html')
+    return render_template('index.html', expected_result="feu", predictions=[["feu",80],["non_feu",20]])
 
 
 if __name__ == "__main__":
