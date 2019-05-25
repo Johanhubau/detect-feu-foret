42ea23a2b1fc510a1d41c4a8dc140341be4d83a7
alexis51151 <alexisdu51151@gmail.com>
2019-05-25 09:29:51 +0200

    Pretrained network with structure VGG16, tested but doesn't work well (overfitting everytime)

--- Multiclass/confusion_matrix.py
+++ Multiclass/confusion_matrix.py
@@ -4,16 +4,16 @@ import seaborn as sn
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-array =  [[39, 0,0,  1,  0],
- [0, 34, 0, 0, 0],
- [ 3,  2, 25,  1,  7],
- [ 1,  1,  0,  5,  0],
- [11,  1, 6, 0,22]]
+array =  [[77, 44, 33,  6],
+ [77, 44, 33, 6],
+ [ 76,  45, 33,  6],
+ [ 77,  44,  33,  6],
+ ]
 
 array = np.array(array)
 array = (array.T/np.sum(array, axis=1)).T
-df_cm = pd.DataFrame(array, ["Fire","Fog","Not Fire","Red object","Smoke"],
-                  ["Fire","Fog","Not Fire","Red object","Smoke"])
+df_cm = pd.DataFrame(array, ["Fire","Smoke","Not Fire","Red object"],
+                  ["Fire","Smoke","Not Fire","Red object"])
 plt.figure(figsize = (10,8))
 sn.set(font_scale=1.4)#for label size
 ax = sn.heatmap(df_cm, annot=True,annot_kws={"size": 16,})# font size
