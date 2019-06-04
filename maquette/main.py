800e271adc21e61e0568c3132d86a37ee6cbc1c6
Johan Hubau <johan.hubau@gmail.com>
2019-06-04 12:19:24 +0200

    Make maquette functionnal

--- maquette/main.py
+++ maquette/main.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template
-import random as rdm
 import matplotlib.image as mpimg
+from PIL import Image
 import numpy as np
+import random as rdm
+from shutil import copyfile
+import glob
+import time
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(256,256,3))
+
+MAIN_PATH = "/home/heisenberg/gate/DB/Bdd_perso/Test/"
+
+class_names = ["Feu", "Fumée ou brouillard", "Non feu", "Objet rouge"]
+class_folder_names = ["Fire/", "Fog/", "Not_fire/", "Red_object/"]
 
 app = Flask(__name__)
-#recup l'image
-def recupimage(path) :
-    img = mpimg.imread(path)
-    if img.dtype == np.float32: # Si le résultat n'est pas un tableau d'entiers
-        img = (img * 255).astype(np.uint8)
-    return img
+
+def read_classes():
+    file = open("net.txt", "r")
+    array = []
+    lines = file.readlines()
+    print(lines)
+    for line in lines:
+        array.append(line[:-1])
+    chosen_class = array.pop()
+    file.close()
+    return array, chosen_class
 
 #l'envoyer à la page et dans le réseau de neuronne(pour l'instant fausse fonction dico)
 def resultat() :
@@ -20,49 +39,76 @@ def resultat() :
     tableau_res['brouillard']=rdm.uniform(0,1)
     return tableau_res
 
-#matrice de confusion sur un fichier
-def stockage(dic) :
-    maxi=0
-    for mot in dic :
-        if dic[mot]> maxi :
-            maxi=dic[mot]
-            maxm=mot
-    return maxm
-
-def ecriture(mot) :
-    fichier = open("backup.txt", "a")
-    fichier.write(mot+" , ")
-    fichier.close()
-    return 
-
 def erase_txt():
     fichier = open("backup.txt", "w")
     fichier.close()
     return
 
-#zoe de test    
-erase_txt()    
-tabl=resultat()
-print(tabl)
-print(stockage(tabl))
-ecriture(stockage(tabl))
-tabl=resultat()
-ecriture(stockage(tabl))
-#fin zone de test
-    
+def get_array():
+    file = open("backup.txt", "r")
+    array = []
+    lines = file.readlines()
+    if lines == []:
+        return [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]
+    for line in lines:
+        line = line[:-1]
+        print(line)
+        subarray = line.split(',')
+        print(subarray)
+        for i in range(len(subarray)):
+            subarray[i] = int(subarray[i])
+        array.append(subarray)
+    file.close()
+    return array
+
+def save_array(array):
+    file = open("backup.txt", "w")
+    for line in array:
+        file.write(str(line[0]) + "," + str(line[1]) + "," + str(line[2]) + "," + str(line[3]) + "\n")
+    file.close()
+
+def make_confusion_matrix(array):
+    array = np.array(array)
+    array = (array.T/np.sum(array, axis=1)).T
+    df_cm = pd.DataFrame(array, ["Fire","Smoke/Fog","Not Fire","Red object"],
+                      ["Fire","Smoke/Fog","Not Fire","Red object"])
+    plt.figure(figsize = (10,8))
+    sn.set(font_scale=1.4)#for label size
+    ax = sn.heatmap(df_cm, annot=True,annot_kws={"size": 16,})# font size
+    plt.xlabel('Classe prédite')
+    plt.ylabel('Vraie classe')
+    plt.savefig("/home/heisenberg/gate/detect-feu-foret/maquette/static/matrix.jpg")
 
 
+@app.route("/")
+def main():
 
-#
-#@app.route("/")
-#def main():
-#
-#    return render_template('index.html')
-#
-#
-#if __name__ == "__main__":
-#    app.run(debug = True)
-#    
-    
-    
+    classes, chosen_class = read_classes()
 
+    max = 0
+    detected = -1
+    #Get detected class
+    print(classes)
+    for i in range(len(classes)):
+        if float(classes[i]) > max:
+            detected = i
+            max = float(classes[1])
+    assert (detected != -1), "Something's wrong with the predictions"
+
+    #Get array, modify it, make confusion matrix and save array
+    array = get_array()
+    array[int(chosen_class)][detected] += 1
+    make_confusion_matrix(array)
+    save_array(array)
+
+    #Sanitize predictions for display
+    predictions = []
+    for i in range(len(classes)):
+        pred = str(float(classes[i])*100) + '%'
+        predictions.append((class_names[i], pred))
+
+    return render_template('index.html', predictions=predictions, expected_result=class_names[int(chosen_class)])
+
+
+if __name__ == "__main__":
+   app.run(debug = True)
--- maquette/utils.py
+++ maquette/utils.py
@@ -4,32 +4,60 @@ import keras
 from keras import Model
 from PIL import Image
 import numpy as np
+import random as rdm
+from shutil import copyfile
+import glob
+import time
 
-
-
-# Here you load your image (must be at least 256 pixels in height and width to work with our neural network format)
-image_path = "/home/alexis51151/detect-feu-foret/Internet/feu12.jpg"
-
-# Here you load your trained network
-path_model="/home/alexis51151/detect-feu-foret/Models/MulticlassV3"
+path_model="/home/heisenberg/gate/models/MulticlassV3"
 model = keras.models.load_model(path_model)
 
-# Your classes
-classes = ["Feu", "Fumée ou brouillard", "Non feu", "Objet rouge"]
+
+MAIN_PATH = "/home/heisenberg/gate/DB/Bdd_perso/Test/"
+
+class_names = ["Feu", "Fumée ou brouillard", "Non feu", "Objet rouge"]
+class_folder_names = ["Fire/", "Fog/", "Not_fire/", "Red_object/"]
 
 def classes_printing(array):
     for i in range(len(array[0])):
-        print("Classe " + classes[i] + " à " + str(array[0][i]*100)+"%")
+        print("Classe " + class_names[i] + " à " + str(array[0][i]*100)+"%")
+
+def classes_write(array, text):
+    file = open("net.txt", "w")
+    for i in range(len(array)):
+        file.write(str(array[i]))
+        file.write("\n")
+    file.write(str(text)+"\n")
+    file.close()
 # Main fonction to ue ; input = path to your image ; output = printing of the prediction and an array
-def classes_from_image(image_path):
+def classes_from_image(image_path, model):
     img = Image.open(image_path)
     img = img.resize((256, 256))
     img = np.array(img)
     img = img.reshape((1, 256, 256, 3))
     res = model.predict(img)
     classes_printing(res)
+    #
+    # y_feature = np.zeros(shape=(1, 8, 8, 512))
+    # y_feature[0] = conv_base.predict(img)
+    # y_feature = np.reshape(y_feature, (1, 8*8*512))
+    # y_prob = model.predict(y_feature)
     return(res[0])
 
-test = classes_from_image(image_path)
 
 
+while True:
+    #Choose random image from random class
+    choose_class = rdm.randint(0,3)
+    image_path = MAIN_PATH + class_folder_names[choose_class]
+    files = glob.glob(image_path + "*")
+    chosen_image = rdm.randint(0, len(files)-1)
+    image_path = files[chosen_image]
+
+    classes = classes_from_image(image_path, model)
+
+    classes_write(classes, choose_class)
+
+    #Copy image
+    copyfile(image_path, "/home/heisenberg/gate/detect-feu-foret/maquette/static/img.jpg")
+    time.sleep(10)
